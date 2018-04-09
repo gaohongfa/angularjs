465013b30bf0c58e7d0e6c9d80ac21c77a84b8a9
高宏发 <826225337@qq.com>
2018-04-09 16:08:30 +0800

    Add files via upload
    资牙首页

---
+++ home/home.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { SubpagePage } from '../subpage/subpage';
+ 
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+
+})
+export class HomePage {
+  items = []; 
+
+  constructor(public navCtrl: NavController) {   
+    for (let i = 0; i < 3; i++) {
+      this.items.push( this.items.length );
+    }
+  }
+
+  doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 3; i++) {
+        this.items.push( this.items.length );
+      }
+
+      console.log('Async operation has ended');
+      console.log(this.items);
+      infiniteScroll.complete();
+      // if (this.items.length > 9) {
+      //   infiniteScroll.enable(false);   //没有数据的时候隐藏 ion-infinate-scroll
+      // }
+      this.items.length=0;
+    }, 500);
+  }
+
+  list="infos";
+
+  goSub(){
+    this.navCtrl.push(SubpagePage,{name:'liu'});
+  }
+}
+
+ 
+ 
+ 
+
+ 
